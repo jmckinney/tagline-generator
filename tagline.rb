5c9adcc4298ba026c23ad4165f3201478c2dfc50
Tim Boisvert <tboisvert@asheavenue.com>
2014-05-13 09:37:23 -0400

    moving arrays into configure block

--- tagline.rb
+++ tagline.rb
@@ -1,51 +1,56 @@
 require 'sinatra'
 
+configure do
+  set :nouns, [
+                "The Cloud", 
+                "disruptor", 
+                "gamification", 
+                "UX",
+                "synergy",
+                "Big Data",
+                "iconoclasts",
+                "nano tech",
+                "crowdsourcing",
+                "b2b marketing",
+                "ROI",
+                "sustainability",
+                "hybrid methodologies"
+              ]
+  
+  set :adjectives,  [
+                      "quantum", 
+                      "scale",
+                      "HTML5", 
+                      "native", 
+                      "disruptive", 
+                      "bleeding edge",
+                      "ephemeral",
+                      "experiential",
+                      "behavioral",
+                      "semantic",
+                      "real-time",
+                      "hyperlocal",
+                      "temporal",
+                      "future-proof"
+                    ]
+
+  set :verbs, [
+                "circling back",
+                "Powering",
+                "Delivering"
+              ]
+                
+  set :sentences, [
+                    "Its the [adjective] level of the [noun]", 
+                    "Bringing your [noun] to the [adjective] world", 
+                    "Powering [noun] one [adjective] step at a time"
+                  ]    
+
+end
+
 get '/' do
-    @noun       = [
-                    "The Cloud", 
-                    "disruptor", 
-                    "gamification", 
-                    "UX",
-                    "synergy",
-                    "Big Data",
-                    "iconoclasts",
-                    "nano tech",
-                    "crowdsourcing",
-                    "b2b marketing",
-                    "ROI",
-                    "sustainability",
-                    "hybrid methodologies"
-                  ]
-
-    @adjective  = [
-                    "quantum", 
-                    "scale",
-                    "HTML5", 
-                    "native", 
-                    "disruptive", 
-                    "bleeding edge",
-                    "ephemeral",
-                    "experiential",
-                    "behavioral",
-                    "semantic",
-                    "real-time",
-                    "hyperlocal",
-                    "temporal",
-                    "future-proof"
-
-                  ]
     
-    @verb       = [
-                    "circling back",
-                    "Powering",
-                    "Delivering"
-                  ]
-    
-    @sentence   = ["Its the [adjective] level of the [noun]", 
-                   "Bringing your [noun] to the [adjective] world", 
-                   "Powering [noun] one [adjective] step at a time"]    
-    
-    @tagline = @sentence.sample.gsub("[adjective]", @adjective.sample).gsub("[noun]", @noun.sample)
+    @tagline = settings.sentences.sample.gsub("[adjective]", settings.adjectives.sample).gsub("[noun]", settings.nouns.sample)
 
     erb :index, :layout => :layout
 
